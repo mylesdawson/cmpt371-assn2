82a685abdf87179de81eb482b0e2c0bbfaa184d0
mylesdawson <mylesdawson@gmail.com>
2018-06-19 15:54:41 -0700

    Sender nearly completed

--- reciever.py
+++ reciever.py
@@ -6,9 +6,18 @@ def generate_random():
     return random.random()
 
 def is_corrupted(input_prob, rand_value):
-    if input_prob < rand_value:
+    if rand_value < input_prob:
         return True
     return False
+
+def decode_data(data):
+    data = data.decode()
+
+    # int_val, seq_num, is_ack, is_nack
+    data = data.split(' ')
+    print(data)
+    return data
+
 
 def main():
     try:
@@ -23,14 +32,18 @@ def main():
     serverHost = '127.0.0.1'
     serverPort = 50007
     # Create a socket that can communicate with ipv4 addresses (AF_INET) using UDP (SOCK_DGRAM)
-    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind((serverHost, serverPort))
+    serverSocket.listen(1)
     print('The server is ready to receive')
 
     # TODO: figure out how to close connection
     while True:
         # TODO: Find out recvfrom port number
-        # sentence, clientAddress = serverSocket.recvfrom()
+        data, clientAddress = serverSocket.accept()
+
+        vals = decode_data(data)
+        is_corrupted = is_corrupted(corrupted_prob, generate_random())
 
     
 
--- sender.py
+++ sender.py
@@ -3,22 +3,44 @@ import random
 import sys
 import time
 
-
-'''simulate random arrival time of packets.  When a packet is sent, 
-the send is followed immediately by a call to this random number generator.  The pseudo 
-random number returned will be interpreted as the delay in seconds before the next 
-packet will be generated.'''
 def rand_arrival_time():
     return random.uniform(0.0, 4.9)
 
-'''used to determine if an ACK and NACK that has just arrived 
-has been corrupted.  This instance of the pseudo random number generator random 
-should generate uniformly distributed pseudo random numbers between [0.0 and 1.0).  
-If the number generated is less that the input value of the probability that an ACK or NACK 
-packet has been be corrupted, then the ACK or NACK packet that has just arrived will be 
-considered to be corrupted.'''
-def rand_corrupted():
-    return random.random
+def rand_corrupted():    
+    return random.random()
+
+def makepkt(int_val, seq_num, is_ack, is_nack):
+    to_encode = [int_val, seq_num, is_ack, is_nack]
+    stringify = [str(x) for x in to_encode]
+    to_string = ' '.join(stringify)
+
+    return to_string.encode()
+
+def decode_res(res):
+    return res.decode().split(' ')
+
+
+def before_messages(seq_num, resent, int_val, is_ack, is_nack):
+    sent_or_resent = resent ? 'resent' : 'sent'
+    ack = is_ack ? 1 : 0
+    nack = is_nack ? 1 : 0
+
+    print('A packet with sequence number {0} is about to be {1}'.format(seq_num, sent_or_resent))
+    print('Packet to send contains: data = {0} seq = {1} ack = {2} nakc = {3}'.format(int_val, seq_num, ack, nack))
+
+def uncorrupted_ack_nack(decode_res):
+    ack = decode_res[2]
+    nack = decode_res[3]
+
+    if ack:
+        print('An ACK packet has just been recieved')
+    else:
+        print('A NACK packet has just been recieved')
+    
+    print('Packet received contains: data = {0} seq = {1} ack = {2} nack = {3}'.format(decode_res[0], decode_res[1], ack, nack))
+
+def state_msg(seq_num):
+    pass
 
 
 def main():
@@ -31,24 +53,49 @@ def main():
         print(type(e).__name__, e.args, '\nPlease provide 4 arguments')
         exit
 
-    client_socket = socket(AF_INET, SOCK_DGRAM)
+    random.seed(arrival_time_seed)
+
+    client_socket = socket(AF_INET, SOCK_STREAM)
     client_port = 2048
     server_name = '127.0.0.1'
     server_port = 50007
 
-    # Send 4 packets (filler for now)
-    packet_list = [0, 0, 0, 0]
+    int_val = 1
+    seq_num = 0
+    is_ack = False
+    is_nack = False
+    resent = False
 
-    # TODO: figure out how ACK and NACK work with packets and sockets
-    
-    for item in packet_list:
-        client_socket.sendto(item, (server_name, server_port))
-        time.sleep(rand_arrival_time()) 
-        
+    client_socket.connect((server_name, server_port))
+
+    for i in range(int(num_packets)):
+        packet = makepkt(int_val, seq_num, is_ack, is_nack)
+        before_messages(seq_num, resent, int_val, is_ack, is_nack)
+        client_socket.send(packet)
+        time.sleep(rand_arrival_time())
+
         returned_message, server_address = client_socket.recvfrom(2048)
+        decoded_res = decode_res(returned_message)
+        uncorrupted_ack_nack(decode_res)
 
+        # Packet is corrupted or recieved a nack
+        while rand_corrupted() < corruption_prob or bool(decode_res[3]):
+            resent = True
+            before_messages(seq_num, resent, int_val, is_ack, is_nack)
+            client_socket.send(packet)
+            returned_message, server_address = client_socket.recvfrom(2048)
+            decoded_res = decode_res(returned_message)
+            uncorrupted_ack_nack(decode_res)
+
+
+        resent = False
+        int_val += 1
+        seq_num = seq_num == 1 ? 0 : 1
+    
     client_socket.close()
+        
 
 
-if __name == '__main__':
+
+if __name__ == '__main__':
     main()
